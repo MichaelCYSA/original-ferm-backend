52fb78522d4931f0caa39e378f9fd1e5662d0380
michael.kysa <michael.kysa@gmail.com>
2023-05-02 05:10:54 -0400

    fix

--- app/models/product.js
+++ app/models/product.js
@@ -1,13 +1,17 @@
 const { Schema, model } = require("mongoose");
 
 const Product = new Schema({
-  name_ru: { type: String },
-  name_ro: { type: String },
-  description_ru: { type: String },
-  description_ro: { type: String },
-  image: { type: String },
-  price: { type: Number },
-  productType: { type: String },
+  description: {
+    ru: { type: Schema.Types.String },
+    ro: { type: Schema.Types.String },
+  },
+  name: {
+    ru: { type: Schema.Types.String },
+    ro: { type: Schema.Types.String },
+  },
+  image: { type: Schema.Types.String },
+  price: { type: Schema.Types.Number },
+  productType: { type: Schema.Types.String },
 });
 
 module.exports = model("Product", Product);
--- app/validation/product.scheme.js
+++ app/validation/product.scheme.js
@@ -1,13 +1,13 @@
 const { body } = require("express-validator");
 
 const productScheme = [
-  body("name_ru").notEmpty(),
-  body("name_ro").notEmpty(),
-  body("description_ru").notEmpty(),
-  body("description_ro").notEmpty(),
+  body("name.ru").notEmpty(),
+  body("name.ro").notEmpty(),
+  body("description.ru").notEmpty(),
+  body("description.ro").notEmpty(),
   body("image").notEmpty(),
   body("price").notEmpty().isNumeric(),
   body("productType").notEmpty(),
 ];
 
-module.exports = productScheme
+module.exports = productScheme;
