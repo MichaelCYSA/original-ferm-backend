585666907bb7e0d44c86b2718b992f5bd6ff2393
michael.kysa <michael.kysa@gmail.com>
2023-05-04 04:48:32 -0400

    added fix

--- app/utils/sendMessageToTelegram.js
+++ app/utils/sendMessageToTelegram.js
@@ -1,6 +1,9 @@
 const TelegramBot = require("node-telegram-bot-api");
 const config = require("../../config.json");
 
+const chatId = process.env.TELEGRAM_CHAT_ID || config.TELEGRAM_CHAT_ID
+const tgToken = process.env.TELEGRAM_KEY || config.TELEGRAM_KEY
+
 module.exports = (data) => {
   const message = `NEW ORDER ! \nAddess: ${data.city} ${
     data.district ? data.district : ""
@@ -10,6 +13,6 @@ module.exports = (data) => {
     data.first_name
   } ${data.last_name} \nTotal Summ: ${data.totalPrice} MDL`;
 
-  const bot = new TelegramBot(config.TELEGRAM_KEY, { polling: true });
-  bot.sendMessage(config.TELEGRAM_CHAT_ID, message);
+  const bot = new TelegramBot(tgToken, { polling: true });
+  bot.sendMessage(chatId, message)
 };
