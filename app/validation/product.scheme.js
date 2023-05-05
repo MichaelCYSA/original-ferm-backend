64112b47ad6bda983eff0b09fe7836626a094779
michael.kysa <michael.kysa@gmail.com>
2023-05-05 08:45:27 -0400

    added c

--- app/validation/product.scheme.js
+++ app/validation/product.scheme.js
@@ -3,8 +3,8 @@ const { body } = require("express-validator");
 const productScheme = [
   body("name.ru").notEmpty(),
   body("name.ro").notEmpty(),
-  body("description.ru").notEmpty(),
-  body("description.ro").notEmpty(),
+  //body("description.ru").notEmpty(),
+  //body("description.ro").notEmpty(),
   body("image").notEmpty(),
   body("price").notEmpty().isNumeric(),
   body("productType").notEmpty(),
